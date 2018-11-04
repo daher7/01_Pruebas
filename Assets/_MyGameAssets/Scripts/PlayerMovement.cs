231934fde5e414a7e3b17c537732a2ea79750655
David Herrera Torrado <39183785+daher7@users.noreply.github.com>
2018-11-04 19:36:07 +0100

    Salto RayCast

--- Assets/_MyGameAssets/Scripts/PlayerMovement.cs
+++ Assets/_MyGameAssets/Scripts/PlayerMovement.cs
@@ -11,10 +11,13 @@ public class PlayerMovement : MonoBehaviour {
     Rigidbody rbPlayer;
     // VARIABLES PARA EL SALTO
     [SerializeField] float jumpForce = 300.0f;
-    [SerializeField] float comprobadorRadio = 0.07f;
-    [SerializeField] Transform comprobadorSuelo;
-    [SerializeField] LayerMask mascaraSuelo;
-    public bool enSuelo = true;
+    [SerializeField] float distanciaSuelo = 0.1f;
+
+    // Si usaramos un OverlapSphere necesitamos estas variables
+    //[SerializeField] float comprobadorRadio = 0.07f;
+    //[SerializeField] Transform comprobadorSuelo;
+    //[SerializeField] LayerMask mascaraSuelo;
+    //public bool enSuelo = true;
 
     void Start () {
 
@@ -23,7 +26,8 @@ public class PlayerMovement : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        enSuelo = Physics2D.OverlapCircle(comprobadorSuelo.position, comprobadorRadio, mascaraSuelo);
+        //enSuelo = Physics2D.OverlapCircle(comprobadorSuelo.position, comprobadorRadio, mascaraSuelo);
+        print(enSuelo());
         rbPlayer.velocity = new Vector2(xPos * speed, rbPlayer.velocity.y);
 
         // Vamos a cambiar de Sentido
@@ -41,18 +45,21 @@ public class PlayerMovement : MonoBehaviour {
 
         xPos = Input.GetAxis("Horizontal");
         // Salto
-        if (enSuelo && Input.GetButtonDown("Jump"))
+        if (enSuelo() && Input.GetButtonDown("Jump"))
         {
             rbPlayer.AddForce(new Vector3(0, jumpForce));
         }
 	}
 
-    
-
     private void DarLaVuelta()
     {
         irDerecha = !irDerecha;
         transform.Rotate(Vector3.up, 180.0f, Space.World);
+    }
+    // Funcion para comprobar que esté en el suelo. Se trata de una funcion que devuelve un valor booleano
+    bool enSuelo()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, distanciaSuelo);
     }
 
     /*
